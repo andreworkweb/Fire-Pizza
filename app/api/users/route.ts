2751183bd962f33f81a243df9c2ef667fc1ba651
Andrew <andreworkweb@gmail.com>
2026-08-15 12:09:35 +0300

    Add database models and relations

---
+++ app/api/users/route.ts
@@ -0,0 +1,14 @@
+import { prisma } from "@/prisma/prisma-client";
+import { NextResponse } from "next/server";
+
+
+export async function GET() {
+    const users = await prisma.user.findMany()
+    return NextResponse.json(users);
+}
+
+
+
+export function POST() {
+
+}
